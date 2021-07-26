273fc0c2d353ec0e8cd7a0f7e3d0d076dec197a9
Alex Breskin <alexdbreskin@gmail.com>
2021-07-26 15:56:55 +1000

    🐛 Speaker will now colouris on first render

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/ImageToggleOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/ImageToggleOnScroll.js
@@ -2,6 +2,9 @@ import React, { useRef, useEffect, useState } from "react";
 
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
@@ -9,6 +12,8 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const [inView, setInView] = useState(false);
 
   useEffect(() => {
+    setIsLoading(false);
+    setInView(isInView());
     window.addEventListener("scroll", scrollHandler);
     return () => {
       window.removeEventListener("scroll", scrollHandler);
@@ -19,7 +24,21 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
     setInView(isInView());
   };
 
-  return <img src={inView ? secondaryImg : primaryImg} alt="" ref={imageRef} />;
+  return (
+    <img
+      src={
+        isLoading
+          ? "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
+    />
+  );
 };
 
 export default ImageToggleOnScroll;
