28eb77583ec4ab652b1c14ffd310521a96c8620c
Alex Breskin <alexdbreskin@gmail.com>
2021-07-26 17:48:52 +1000

    ✨ Added eslint and experimented

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const InputElement = () => {
   const [inputText, setInputText] = useState("");
   const [historyList, setHistoryList] = useState([]);
 
-  return (
+  const random_boolean = Math.random() >= 0.5;
+  const [isLoading, setIsLoading] = useState(random_boolean === true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  });
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
     <div>
       <input
         onChange={(e) => {
