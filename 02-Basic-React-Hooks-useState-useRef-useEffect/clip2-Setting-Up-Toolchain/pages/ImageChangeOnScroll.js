1a407d5fef11e8966e2c08766364cc8510a82c09
Alex Breskin <alexdbreskin@gmail.com>
2021-07-26 16:03:47 +1000

    ✨ Will now show speakerId on title

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/ImageChangeOnScroll.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/ImageChangeOnScroll.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageToggleOnScroll from "../src/ImageToggleOnScroll";
 
 const ImageChangeOnScroll = () => {
+  const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+  useEffect(() => {
+    window.document.title = `SpeakerId: ${currentSpeakerId}`;
+    console.log("useEffect: setting title to ", currentSpeakerId);
+  });
   return (
     <div>
+      <span>mouseEventCnt: ${mouseEventCnt}</span>
       {[1124, 187, 823, 1269, 1530].map((speakerId) => {
         return (
-          <div key={speakerId}>
+          <div
+            key={speakerId}
+            onMouseOver={() => {
+              setCurrentSpeakerId(speakerId);
+              setMouseEventCnt(mouseEventCnt + 1);
+              console.log("onMouseOver:", speakerId);
+            }}
+          >
             <ImageToggleOnScroll
               primaryImg={`/static/speakers/bw/Speaker-${speakerId}.jpg`}
               secondaryImg={`/static/speakers/Speaker-${speakerId}.jpg`}
