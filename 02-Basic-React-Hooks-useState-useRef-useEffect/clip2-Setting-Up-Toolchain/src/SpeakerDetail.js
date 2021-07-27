d00344a4347a6788ba17720f04fff8f40bdba234
Alex Breskin <alexdbreskin@gmail.com>
2021-07-27 16:14:15 +1000

    ⚡️ Used Callbacks and memos for performance

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/SpeakerDetail.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/SpeakerDetail.js
@@ -1,38 +1,35 @@
 import ImageToggleOnScroll from "./ImageToggleOnScroll";
+import React from "react";
 
-const SpeakerDetail = ({
-  id,
-  firstName,
-  lastName,
-  favourite,
-  bio,
-  onHeartFavouriteHandler,
-}) => {
-  return (
-    <div className="card col-4 cardmin">
-      <ImageToggleOnScroll
-        className="card-img-top"
-        primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
-        secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
-        alt={`${firstName} ${lastName}}`}
-      />
-      <div className="card-body">
-        <h4 className="card-title">
-          <button
-            data-sessionid={id}
-            className={favourite ? "heartredbutton" : "heartdarkbutton"}
-            onClick={(e) => {
-              onHeartFavouriteHandler(e, !favourite);
-            }}
-          />
-          <span>
-            {firstName} {lastName}
-          </span>
-        </h4>
-        <span>{bio}</span>
+const SpeakerDetail = React.memo(
+  ({ id, firstName, lastName, favourite, bio, onHeartFavouriteHandler }) => {
+    // console.log("SpeakeDetail:", id, firstName, lastName, favourite);
+    return (
+      <div className="card col-4 cardmin">
+        <ImageToggleOnScroll
+          className="card-img-top"
+          primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
+          secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
+          alt={`${firstName} ${lastName}}`}
+        />
+        <div className="card-body">
+          <h4 className="card-title">
+            <button
+              data-sessionid={id}
+              className={favourite ? "heartredbutton" : "heartdarkbutton"}
+              onClick={(e) => {
+                onHeartFavouriteHandler(e, !favourite);
+              }}
+            />
+            <span>
+              {firstName} {lastName}
+            </span>
+          </h4>
+          <span>{bio}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default SpeakerDetail;
