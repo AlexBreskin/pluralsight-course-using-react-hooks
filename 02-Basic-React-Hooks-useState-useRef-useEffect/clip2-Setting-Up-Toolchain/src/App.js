1fba44349cb76743d31372105170f6a25a2c8cc8
Alex Breskin <alexdbreskin@gmail.com>
2021-07-27 10:02:42 +1000

    🚧 Added ConfigContext

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/App.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/src/App.js
@@ -2,10 +2,24 @@ import React from "react";
 import Home from "./Home.js";
 import Speakers from "./Speakers.js";
 
-const App = ({ pageName }) => {
+export const ConfigContext = React.createContext();
+
+const pageToShow = (pageName) => {
   if (pageName === "Home") return <Home></Home>;
   if (pageName === "Speakers") return <Speakers></Speakers>;
   return <div>Not Found</div>;
+};
+
+const configValue = {
+  showSpeakerSpeakingDays: true,
+};
+
+const App = ({ pageName }) => {
+  return (
+    <ConfigContext.Provider value={configValue}>
+      <div>{pageToShow(pageName)}</div>
+    </ConfigContext.Provider>
+  );
 };
 
 export default App;
